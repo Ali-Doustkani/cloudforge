84bfdcc3f9bb4192ca24734bdde4aba6ab12277c
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-17 21:54:46 +0100

    fix

--- .github/workflows/scripts/infra-test.sh
+++ .github/workflows/scripts/infra-test.sh
@@ -9,7 +9,7 @@ acrName=$(jq --raw-output ".acrName.value" $bicepOutputPath)
 appServiceName=$(jq --raw-output ".appServiceName.value" $bicepOutputPath)
 
 echo "Importing test image into registry '$acrName'"
-az acr import --name $acrName --source docker.io/alidoustkani/infratest:6 --image infratest:testversion
+az acr import --name $acrName --source ghcr.io/ali-doustkani/infratest:latest --image infratest:testversion
 
 echo "Restarting app '$appServiceName'"
 az webapp restart --name $appServiceName --resource-group $groupName
