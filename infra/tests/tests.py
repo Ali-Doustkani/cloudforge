b2a6e6afb3921e26e47647dfdcf3554689fc721c
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-18 19:26:10 +0100

    fix sh left over

--- infra/tests/tests.py
+++ infra/tests/tests.py
@@ -4,7 +4,7 @@ import requests
 assert len(sys.argv) == 2, "Expected to get webapp name URL as input"
 
 appServiceName = sys.argv[1]
-url = f"https://${appServiceName}.azurewebsites.net"
+url = f"https://{appServiceName}.azurewebsites.net"
 
 response = requests.get(url)
 assert response.status_code == 200, f"Expected HTTP Status Code 200 but received {response.status_code}"
