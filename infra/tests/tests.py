1d6b437c99c92e14707adb93d908e511e9fded12
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-18 21:08:15 +0100

    test app config

--- infra/tests/tests.py
+++ infra/tests/tests.py
@@ -10,3 +10,5 @@ response = requests.get(url)
 assert response.status_code == 200, f"Expected HTTP Status Code 200 but received {response.status_code}"
 
 assert "HTTP OK" in response.text, f"Expected response to contain 'HTTP OK' but was '{response.text}'"
+
+assert "App Config: infra_value" in response.text f"Expected response to contain test value but contained '{response.text}'"
