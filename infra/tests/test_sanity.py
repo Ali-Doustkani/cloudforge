1965a3a596d799f8516fa59fff46f92906ff0eb1
Ali Doustkani <ali.doostkany@gmail.com>
2026-01-12 19:50:08 +0100

    convert all tests to pytest

--- infra/tests/test_sanity.py
+++ infra/tests/test_sanity.py
@@ -1,23 +1,20 @@
 import pytest
 import requests
 
-# assert len(sys.argv) == 2, "Expected to get webapp name URL as input"
-
-# url = sys.argv[1]
-# print(f"Testing '{url}'")
-
-def test_http_ok(url):
-    response = requests.get(url)
-    assert response.status_code == 200, f"Expected HTTP Status Code 200 but received {response.status_code}"
-
 @pytest.fixture
 def url(pytestconfig):
     return pytestconfig.getoption("url")
 
-# assert "HTTP OK" in response.text, f"Expected response to contain 'HTTP OK' but was '{response.text}'"
+def test_http_ok(url):
+    response = requests.get(url)
+    assert response.status_code == 200, f"Expected HTTP Status Code 200 but received {response.status_code}"
+    assert "HTTP OK" in response.text, f"Expected response to contain 'HTTP OK' but was '{response.text}'"
 
-# assert "App Config: infra_value" in response.text, f"Expected response to contain 'App Config: infra_value' but was '{response.text}'"
+def test_appconfig(url):
+    response = requests.get(url)
+    assert "App Config: infra_value" in response.text, f"Expected response to contain 'App Config: infra_value' but was '{response.text}'"
 
-# assert "Key Vault: infra_value" in response.text, f"Expected response to contain 'Key Vault: infra_value' but was '{response.text}'"
-
-# assert "Set Secret: denied" in response.text, f"Expected response to contain 'Set Secret: denied' but was '{response.text}'"    
+def test_kv(url):
+    response = requests.get(url)
+    assert "Key Vault: infra_value" in response.text, f"Expected response to contain 'Key Vault: infra_value' but was '{response.text}'"
+    assert "Set Secret: denied" in response.text, f"Expected response to contain 'Set Secret: denied' but was '{response.text}'"    
