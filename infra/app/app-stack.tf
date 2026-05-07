eaa618c88e1fa95a7b19ce4cb3c12f7e517166d2
Ali Doustkani <ali.doostkany@gmail.com>
2026-05-07 20:29:12 +0200

    fix

--- infra/app/app-stack.tf
+++ infra/app/app-stack.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.62"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>3.4"
+    }
   }
   backend "azurerm" {
     resource_group_name = "rg-bootstrap"
@@ -16,7 +20,33 @@ provider "azurerm" {
   features {}
 }
 
+provider "azuread" {}
+
+data "azuread_service_principal" "github" {
+  display_name = "github"
+}
+
 resource "azurerm_resource_group" "app" {
   name     = "rg-cloudforge"
   location = "austriaeast"
+}
+
+resource "azurerm_storage_account" "status" {
+  name                     = "stcloudforgestatus"
+  resource_group_name      = azurerm_resource_group.app.name
+  location                 = azurerm_resource_group.app.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account_static_website" "static_web" {
+  storage_account_id = azurerm_storage_account.status.id
+  index_document     = "index.html"
+  error_404_document = "error.html"
+}
+
+resource "azurerm_role_assignment" "github_actions_storage" {
+  scope                = azurerm_storage_account.status.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azuread_service_principal.github.object_id
 }
