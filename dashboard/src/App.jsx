f718877e2b94f806f0cf1ab12e03250eb3dbcc47
Ali Doustkani <ali.doostkany@gmail.com>
2026-05-12 21:32:40 +0200

    fix

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -5,16 +5,17 @@ import Health from './Health'
 import NotFound from './NotFound'
 import Login from './Login'
 
-export default function App(){
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/instances" element={<Instances />}/>
-          <Route path="/health" element={<Health/>}/>
-          <Route path="/not-found" element={<NotFound />}/>
-          <Route path="/login" element={<Login/>}/>
-        </Routes>
-      </BrowserRouter>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/instances" element={<Instances/>}/>
+                <Route path="/health" element={<Health/>}/>
+                <Route path="/not-found" element={<NotFound/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
